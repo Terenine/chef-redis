ec616adbeed3eccbc17a9e091546937564ec4641
Chris Meadows <meadoch1@gmail.com>
2013-01-06 14:31:35 -0500

    Adding chkconfig compatibility to source build

--- recipes/source.rb
+++ recipes/source.rb
@@ -80,6 +80,15 @@ if node['redis']['source']['create_service']
       cp #{cache_dir}/#{tar_dir}/utils/redis_init_script /etc/init.d/redis
     COMMAND
 
+    ruby_block "insert_line" do
+      block do
+        file = Chef::Util::FileEdit.new("/etc/init.d/redis")
+        file.insert_line_after_match("/^#\!\/bin\//", "#   chkconfig: 2345 90 10")
+        file.write_file
+      end
+    end
+    
+
     creates   "/etc/init.d/redis"
   end
 
